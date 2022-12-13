cae429e216722eeb2430a49d26d0690182e23c7d
rajesh-pathak <rajesh.pathak@pnp-hcl.com>
2022-12-13 13:38:46 +0530

    firefox browser support added

--- wdio.conf.ts
+++ wdio.conf.ts
@@ -108,6 +108,12 @@ export const config: Options.Testrunner = {
       // excludeDriverLogs: ['*'], // pass '*' to exclude all driver session logs
       // excludeDriverLogs: ['bugreport', 'server'],
     },
+    {
+      maxInstances: 3,
+      //
+      browserName: "firefox",
+      acceptInsecureCerts: true,
+    },
   ],
   //
   // ===================
@@ -156,7 +162,7 @@ export const config: Options.Testrunner = {
   // Services take over a specific job you don't want to take care of. They enhance
   // your test setup with almost no effort. Unlike plugins, they don't add new
   // commands. Instead, they hook themselves up into the test process.
-  services: ["chromedriver"],
+  services: ["chromedriver", "geckodriver"],
 
   // Framework you want to run your specs with.
   // The following are supported: Mocha, Jasmine, and Cucumber
